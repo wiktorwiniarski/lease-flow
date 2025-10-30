1e3eba15039e027207adad3bc1c044d12510c7a0
wiktorwiniarski <130755152+wiktorwiniarski@users.noreply.github.com>
2025-10-30 10:29:16 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
