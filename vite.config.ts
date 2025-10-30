3814e9430abad922126c2ce1f7d6476fdc1e109e
wiktorwiniarski <130755152+wiktorwiniarski@users.noreply.github.com>
2025-10-30 10:31:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH || "/lease-flow",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
